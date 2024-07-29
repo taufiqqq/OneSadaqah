01578a8c0510ae6d32530db964b1820e0cb482ae
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2024-07-29 21:52:25 +0800

    Create config.php

---
+++ one-sadaqah/database/config.php
@@ -0,0 +1,17 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'hackathon');
+
+
+function getDBConnection()
+{
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+
+?>
