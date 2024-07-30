9cecd33babb87a021752d1584c6480e2ca8c886a
LUQMAN HAKIM <111760088+luqmanhakim02@users.noreply.github.com>
2024-07-30 13:41:29 +0800

    get Organization by Id is available

---
+++ one-sadaqah/database/get_Organization.php
@@ -0,0 +1,28 @@
+<?php
+include 'config.php';
+
+header('Content-Type: application/json');
+
+//Database connection
+$conn = getDbConnection();
+
+// Fetch all organization
+$sql = "SELECT * FROM organization";
+$result = $conn->query($sql);
+$organization = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $organization[] = $row;
+    }
+} else {
+    echo json_encode([]);
+}
+echo json_encode($organization);
+
+
+
+
+$conn->close();
+
+?>
