439856b428ebea27e31d05997ab54f0023e49481
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2024-08-23 10:10:08 +0800

    update config

---
+++ one-sadaqah/config.js
@@ -0,0 +1,10 @@
+window.env = {
+    client_id: "58e89fe45994e7af5d0c154d2f4f7b3b004bb05832568483d6a86424301d695e",
+    client_secret: "sk_7c2641de536c5a69db69c72a31b6bd85ca164c883882ad491d8c2231fdcd8efc",
+    wallet_address:"0x972E7c410C776dB363c9c282F51D280A61560202",
+    transferToAddress1:"0x3F2390100afd70455Cf007172c3dbE1c0097A481",
+    wallet_address2:"0xC0d15De045fDbCfCC842e6A708F0a34233D60077",
+    wallet_address3:"0x2FA0BE0943D74E49039ce68faB39FACD2847DCaB",
+    transferContractAddress:"0xFAAF45A188c15687Ad0372e15Ba077bcB7E4615f",
+    callbackUrl:"taufiqqq.github.io",
+};
