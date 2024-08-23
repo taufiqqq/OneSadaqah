4b294c0c6b40bd39fe0d51c8c7fee879dd0e463d
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2024-08-23 10:21:11 +0800

    Update config.js

--- one-sadaqah/config.js
+++ one-sadaqah/config.js
@@ -1,3 +1,9 @@
+//Yes, I know you are trying to find vulnerability in this app
+//Cmon thats not cool...
+//Can you like ignore this? I mean we havent implemented the real transaction and account creation
+//So I dont see any problem with... exposing some stuff? LOL
+//Anyway pls ignore this, thank you very much
+
 window.env = {
     client_id: "58e89fe45994e7af5d0c154d2f4f7b3b004bb05832568483d6a86424301d695e",
     client_secret: "sk_7c2641de536c5a69db69c72a31b6bd85ca164c883882ad491d8c2231fdcd8efc",
